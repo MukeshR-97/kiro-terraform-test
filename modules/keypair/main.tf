636c76b27df6d4d6e5f17cc7e48a4eb0a8f7f2ab
MukeshR-97 <mukesh.r@ilios.digital>
2026-04-28 18:36:04 +0530

    FULL terraform sync

---
+++ modules/keypair/main.tf
@@ -0,0 +1,19 @@
+# Generate RSA private key
+resource "tls_private_key" "this" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+# Create AWS Key Pair using public key
+resource "aws_key_pair" "this" {
+  key_name   = var.key_name
+  public_key = tls_private_key.this.public_key_openssh
+}
+
+# Save private key locally
+resource "local_file" "private_key" {
+  content  = tls_private_key.this.private_key_pem
+  filename = "${var.key_name}.pem"
+
+  file_permission = "0400"
+}
